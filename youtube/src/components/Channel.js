bead0fe777bbedebaafbd588ef2e0db7ef22bd30
Sanjay Sharma <sharma.sanjay813@gmail.com>
2024-09-27 21:31:54 +0530

    Channel page made better, displays image name description

--- youtube/src/components/Channel.js
+++ youtube/src/components/Channel.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom';
 
 const Channel = () => {
+    const [searchParams] = useSearchParams();
+    const [channelInformation, setChannelInformation] = useState({});
     async function channelInfo () {
-        const data = await fetch("'https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=[YOUR_API_KEY]'");
-        // setup
+        const channelId = searchParams.get('v');
+        const data = await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${channelId}&key=${process.env.REACT_APP_YOUTUBE_API}`);
+        const json = await data.json();
+        setChannelInformation(json.items[0]);
     }
+
+    useEffect(() => {
+        channelInfo();
+    },[]);
     return (
-    <div>
-        <image src="" alt="Channel Logo" className="channel-logo"/>
-        <div>
-            channel info
+        channelInformation.snippet &&
+        <div className="max-w-4xl mx-auto p-4 bg-white shadow-lg rounded-lg">
+            <img src={channelInformation.snippet?.thumbnails?.high.url} alt="Channel Logo" className="w-32 h-32 rounded-full mx-auto"/>
+            <div className="text-center mt-4">
+                <h1 className="text-2xl font-bold text-gray-800">{channelInformation.snippet?.title}</h1>
+                <p className="mt-2 text-gray-600">{channelInformation.snippet?.description}</p>
+            </div>
         </div>
-        <div>
-            Menu items
-        </div>
-    </div>
     )
 }
 
